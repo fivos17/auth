08388deb280ea3e46174d8c08457c7c8e6b17a7a
fivo <fivofouka@gmail.com>
2021-12-15 18:49:38 +0200

    changes to github auth

--- auth.js
+++ auth.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const passport = require('passport');
 const ObjectID = require('mongodb').ObjectID;
 const LocalStrategy = require('passport-local');
-const GitHubStrategy = require('passport-github');
+const GitHubStrategy = require('passport-github').Strategy;
 const bcrypt = require('bcrypt');
 
 module.exports = function (app, myDataBase) {
@@ -9,14 +10,9 @@ module.exports = function (app, myDataBase) {
         clientID: process.env.GITHUB_CLIENT_ID,
         clientSecret: process.env.GITHUB_CLIENT_SECRET,
         callbackURL: 'https://auth-fcc.herokuapp.com/auth/github/callback'},
-        function(username, password, done){
-            myDataBase.findOne({ username: username }, function (err, user) {
-                console.log('User ' + username + ' attempted to log in.');
-                if (err) { return done(err); }
-                if (!user) { return done(null, false); }
-                if (!bcrypt.compareSync(password, user.password)) { return done(null, false); }
-                return done(null, user);
-            });
+        function(accessToken, refreshToken, profile, cb){
+            console.log(profile);
+            // DB
         })
     );
     passport.use(new LocalStrategy(
